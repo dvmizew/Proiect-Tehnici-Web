5d488baeb53ff803bc7f8986759d2408e4fe4c10
dvmizew <dvmiradu@gmail.com>
2024-03-25 12:08:17 +0200

    accesare din node

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ app.set("view engine", "ejs");
 app.use("/resurse", express.static(__dirname + "/resurse"));
 app.listen(8080);
 console.log("Serverul a pornit");
+
+// app.get("/", function(req, res) {
+//     res.sendFile(__dirname + "/index.html");
+// })
+
+app.get(["/", "/home", "/index"], function (req, res) {
+    res.render("pagini/index");
+})
+
+app.get("/data", function (req, res, next) {
+    res.write("Data: ");
+    next();
+})
+
+app.get("/data", function (req, res) {
+    res.write("" + new Date());
+    res.end();
+})
+
+app.get("/suma/:a/:b", function (req, res) {
+    var suma = parseInt(req.params.a) + parseInt(req.params.b);
+    res.send("" + suma);
+})
+
+app.get("/*", function(req, res){
+    console.log(req.url)
+    res.render("pagini" + req.url, function(err, rezHtml){
+        console.log(rezHtml);
+        console.log("Eroare: " + err)
+        res.send(rezHtml + "");
+    });
+})
